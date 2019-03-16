ce99bb33fe660849f95c170c7e6d06372029f6d3
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-16 20:37:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,8 @@
   def speak_to_grandma(phrase)
       if !phrase.upcase
         return "HUH?! SPEAK UP, SONNY!"
-        elsif phrase.upcase 
+      elsif phrase.upcase
+        return "NO, NOT SINCE 1938!"
   end 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
