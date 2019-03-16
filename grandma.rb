93b84595c6e4c2211cf4b8a650adcdda54019d96
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-16 20:47:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,9 @@
   def speak_to_grandma(phrase)
       if phrase != phrase.upcase
         return "HUH?! SPEAK UP, SONNY!"
+        
+      if phrase == phrase.upcase
+        return "NO, NOT SINCE 1938!"
       end 
   end 
 # Whatever you say to grandma, she should respond with
