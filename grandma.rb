74b26a3db48c1cc0c5d56c1996cf7fe3fd77f197
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-16 20:42:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,5 @@
 # Write a speak_to_grandma method.
+  require 'pry'
   def speak_to_grandma(phrase)
       if phrase == phrase.downcase
         return "HUH?! SPEAK UP, SONNY!"
@@ -6,6 +7,7 @@
         return "NO, NOT SINCE 1938!"
       else phrase== "I LOVE YOU GRANDMA!"
         return "I LOVE YOU TOO PUMPKIN!"
+        binding.pry
       end 
   end 
 # Whatever you say to grandma, she should respond with
