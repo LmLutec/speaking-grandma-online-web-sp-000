02d7da17651cf0c3b31af414cd89af6936f98aac
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-16 20:44:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@
       elsif phrase == phrase.upcase
         return "NO, NOT SINCE 1938!"
         binding.pry 
-      else phrase== "I LOVE YOU GRANDMA!"
+      elsif phrase== "I LOVE YOU GRANDMA!"
         return "I LOVE YOU TOO PUMPKIN!"
       end 
   end 
