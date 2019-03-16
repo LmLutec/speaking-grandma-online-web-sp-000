029f9487228d7268b19380af1a06068838bcc33e
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-16 20:50:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
       if phrase != phrase.upcase
         return "HUH?! SPEAK UP, SONNY!"
         
-      elsif phrase == phrase.upcase
+      elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
         return "NO, NOT SINCE 1938!"
         
       elsif phrase == "I LOVE YOU GRANDMA!"
