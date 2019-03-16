ac6b393516a2988fb32ab671a81ff7bf1a29e575
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-16 20:46:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
   require 'pry'
   def speak_to_grandma(phrase)
-      if phrase == phrase.downcase
+      if phrase != phrase.upcase
         return "HUH?! SPEAK UP, SONNY!"
       end 
   end 
